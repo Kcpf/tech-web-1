09c5c59a663465fa32a3748e90921aa304166005
Kcpf <fe22122002@gmail.com>
2021-10-19 15:05:57 -0300

    adding functional api

--- Projeto-B/notes/urls.py
+++ Projeto-B/notes/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('api/notes/<int:note_id>/', views.api_note),
+    path('api/notes/', views.api_notes),
     path('details', views.details, name="details"),
     path('tags', views.tags, name="tags"),
     path('update', views.update, name="update"),
